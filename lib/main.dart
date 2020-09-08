073b07495cb756726177bed1c0710cc568f331d0
ngirimana <chadrackngirimana@gmail.com>
2020-09-08 10:34:00 +0200

    add state

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO:implement createState
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('My First App')), //appBar
+      body: Column(
+        children: [
+          Text(questions[questionIndex]), //Text
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 choosen')),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () => print('Answer 3 choosen'),
+          ),
+        ],
+      ),
+    ));
   }
 }
